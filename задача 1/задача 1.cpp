135d456d4515bb76fb64a8afd349efbd5185e987
Alexandra <alekmum@yandex.ru>
2026-02-26 13:20:13 +0300

    Add files via upload
    Добавление файлов во ВторуюВетвь репозитория

---
+++ задача 1/задача 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    double a, b, c, P, S, r;
+    cout << "Введите сторону a: "; cin >> a;
+    cout << "Введите сторону b: "; cin >> b;
+    cout << "Введите сторону c: "; cin >> c;
+    P = a + b + c;
+    r = P / 2;
+    S = sqrt(r * (r - a) * (r - b) * (r - c));
+    cout << "Периметр P = " << P << endl;
+    cout << "Площадь S = " << S << endl;
+    system("pause");
+    return 0;
+}
